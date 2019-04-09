851f44a6dce8444c62e117d2705f09c4f6221424
Ben <rbenrodford@gmail.com>
2019-04-08 23:05:21 -0400

    started planning a bit

---
+++ sched.py
@@ -0,0 +1,17 @@
+'''
+Scheduling algorithm implementation
+Ben Rodford
+April 2019
+'''
+
+'''
+node v1:
+    data
+    outbound edges
+        (v2, capacity, flow)
+    inbound edges
+        (v0, capacity)?
+        used for residual graph
+'''
+
+
